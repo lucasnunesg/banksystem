c45d0f649497817a461fb6dcebad03a068e37712
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-09-29 20:37:27 -0300

    feat: added `createAccount` functionality

--- src/main/java/com/lucasnunesg/banksystem/services/AccountService.java
+++ src/main/java/com/lucasnunesg/banksystem/services/AccountService.java
@@ -1,22 +1,43 @@
 package com.lucasnunesg.banksystem.services;
 
+import com.lucasnunesg.banksystem.controllers.dto.CreateAccountDto;
 import com.lucasnunesg.banksystem.entities.Account;
-import com.lucasnunesg.banksystem.entities.PersonalAccount;
 import com.lucasnunesg.banksystem.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public abstract class AccountService {
+public class AccountService {
+
 
     private final AccountRepository repository;
 
     @Autowired
-    protected AccountService(AccountRepository repository) {
+    public AccountService(AccountRepository repository) {
         this.repository = repository;
     }
 
-    protected Account createAccount(Account obj) {
+    public Account createAccount(CreateAccountDto dto) {
+        var account = repository.findByEmailOrDocument(dto.email(),dto.document());
+        if (account.isPresent()) {
+            throw new UnsupportedOperationException("Account already exists");
+        }
+        return repository.save(dto.toEntity());
+    }
+
+    public List<Account> findAll() {
+        return repository.findAll();
+    }
+
+    public Account findById(Long id) {
+        Optional<Account> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new IllegalArgumentException("User not found:" + id));
+    }
+
+    public Account insert(Account obj) {
         return repository.save(obj);
     }
 
