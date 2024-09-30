eae4c0195353b75a201dc6bdfbb8939e91baaa44
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-09-30 14:03:25 -0300

    feat: created `Transfer` entity and repository

--- src/main/java/com/lucasnunesg/banksystem/config/DatabaseSeed.java
+++ src/main/java/com/lucasnunesg/banksystem/config/DatabaseSeed.java
@@ -2,7 +2,9 @@ package com.lucasnunesg.banksystem.config;
 
 import com.lucasnunesg.banksystem.entities.BusinessAccount;
 import com.lucasnunesg.banksystem.entities.PersonalAccount;
+import com.lucasnunesg.banksystem.entities.Transfer;
 import com.lucasnunesg.banksystem.repositories.AccountRepository;
+import com.lucasnunesg.banksystem.repositories.TransferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,14 @@ import java.util.Arrays;
 
 @Configuration
 public class DatabaseSeed implements CommandLineRunner {
+
     private final AccountRepository accountRepository;
+    private final TransferRepository transferRepository;
 
     @Autowired
-    public DatabaseSeed(AccountRepository accountRepository){
+    public DatabaseSeed(AccountRepository accountRepository, TransferRepository transferRepository){
         this.accountRepository = accountRepository;
+        this.transferRepository = transferRepository;
     }
 
     @Override
@@ -36,6 +41,10 @@ public class DatabaseSeed implements CommandLineRunner {
         ba1.setBalance(BigDecimal.valueOf(5000.00));
         pa1.setBalance(BigDecimal.valueOf(3000.00));
 
+        Transfer t1 = new Transfer(pa1,ba1,BigDecimal.valueOf(200.00));
+        Transfer t2 = new Transfer(pa1,ba1,BigDecimal.valueOf(500.00));
+
         accountRepository.saveAll(Arrays.asList(pa1, ba1));
+        transferRepository.saveAll(Arrays.asList(t1,t2));
     }
 }
---
+++ src/main/java/com/lucasnunesg/banksystem/entities/Transfer.java
@@ -0,0 +1,71 @@
+package com.lucasnunesg.banksystem.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transfers")
+public class Transfer implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_account_id")
+    private Account sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_account_id")
+    private Account receiver;
+
+    private BigDecimal amount;
+
+    public Transfer(){}
+
+    public Transfer(Account sender, Account receiver, BigDecimal amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Account getSender() {
+        return sender;
+    }
+
+    public void setSender(Account sender) {
+        this.sender = sender;
+    }
+
+    public Account getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Account receiver) {
+        this.receiver = receiver;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
