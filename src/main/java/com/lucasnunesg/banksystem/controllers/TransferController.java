6496112fceacbfef1a3ef8109e56f985a0e51b0a
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-09-30 14:08:53 -0300

    feat: created `Transfer` controller

---
+++ src/main/java/com/lucasnunesg/banksystem/controllers/TransferController.java
@@ -0,0 +1,46 @@
+package com.lucasnunesg.banksystem.controllers;
+
+import com.lucasnunesg.banksystem.controllers.dto.CreateAccountDto;
+import com.lucasnunesg.banksystem.controllers.dto.TransferDto;
+import com.lucasnunesg.banksystem.entities.Account;
+import com.lucasnunesg.banksystem.entities.Transfer;
+import com.lucasnunesg.banksystem.services.AccountService;
+import com.lucasnunesg.banksystem.services.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/transfers")
+public class TransferController {
+
+    private final TransferService service;
+
+    @Autowired
+    public TransferController(TransferService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transfer>> findAll() {
+        List<Transfer> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Transfer> findById(@PathVariable Long id) {
+        Transfer obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transfer> transfer(@RequestBody TransferDto dto) {
+        var transfer = service.transfer(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(transfer.getId()).toUri();
+        return ResponseEntity.created(uri).body(transfer);
+    }
+}
