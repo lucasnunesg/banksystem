c931e3a529bba695a80bb4cf41d4c7d90f74e958
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-10-01 12:12:49 -0300

    feat: added `InvalidAccountTypeException`

---
+++ src/main/java/com/lucasnunesg/banksystem/exceptions/InvalidAccountTypeException.java
@@ -0,0 +1,23 @@
+package com.lucasnunesg.banksystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class InvalidAccountTypeException extends BankSystemGenericException{
+
+    private final String details;
+
+    public InvalidAccountTypeException(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public ProblemDetail createProblemDetail() {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        problemDetail.setTitle("Invalid account type");
+        problemDetail.setDetail(details);
+
+        return problemDetail;
+    }
+}
