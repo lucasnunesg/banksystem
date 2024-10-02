18cc37e9e82e59edc2def41f18b69b018bdbd9b0
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-10-01 23:47:07 -0300

    refactor: renamed endpoint to meet contract

--- src/main/java/com/lucasnunesg/banksystem/controllers/TransferController.java
+++ src/main/java/com/lucasnunesg/banksystem/controllers/TransferController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/transfers")
+@RequestMapping(value = "/transfer")
 public class TransferController {
 
     private final TransferService service;
